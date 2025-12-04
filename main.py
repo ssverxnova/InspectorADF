6b2ee0ee38e620c59c47ab102aff3ebb86f40b3b
ssverxnova <151632186+ssverxnova@users.noreply.github.com>
2025-12-04 16:37:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-from bot import *
+from bot import run_bot, run_flask
+import threading
+
+# Запуск Flask в отдельном потоке
+threading.Thread(target=run_flask).start()
+
+# Запуск Telegram-бота
+run_bot()
